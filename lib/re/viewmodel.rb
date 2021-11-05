f9860f31482f7516805748fb2286d4ad80c107bb
Vidar Hokstad <vidar@hokstad.com>
2021-11-05 17:20:26 +0000

    Calculate x position in the face of tabs

--- lib/re/viewmodel.rb
+++ lib/re/viewmodel.rb
@@ -1,16 +1,15 @@
-
-# # ViewModel
+# ViewModel
 #
 # The model that represents the actual file data is Buffer
-# 
+#
 # However, the model may not pecisely reflect how the data
-# should be presented. The most basic example is that 
+# should be presented. The most basic example is that
 # tab characters are represented as a variable number of
 # characters on screen.
 #
 # The ViewModel's job is to be an intermediary, translating
 # operations on the *visible* part of the buffer into operations
-# on the Buffer. 
+# on the Buffer.
 #
 # This includes cursor movement and editing operations.
 #
@@ -35,21 +34,37 @@ class ViewModel
     @editor.cursor.row
   end
 
+  def cursor_x(row,col)
+    return 0 if col < 1
+    l = @editor.buffer.lines(row)
+    pos = 0
+    (0..(col-1)).each do |i|
+      if l[i].to_s == "\t"
+        @editor.message = i.to_s
+        pos += 4-(pos % 4)
+      else
+        pos += 1
+      end
+    end
+    return pos
+  end
+
   def right(offset = 1)
     old = @editor.cursor
     c = old.col
     r = old.row
     l = @editor.view.update_line(r)
+    return old if !l
     if l[c].to_s == TABCHAR
       c += 1
-      max = c + c % 4 + 1
+      max = c + (4 - c % 4)
       while l[c].to_s == TABCHAR and c <= max
         c += 1
       end
       return Cursor.new(r,c)
     else
       if c >= l.length
-        return old if r >= buffer.lines_count
+        return old if r+1 >= buffer.lines_count
         Cursor.new(r+1,0)
       else
         return Cursor.new(r,c+offset)
